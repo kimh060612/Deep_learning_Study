f04d673bb82aa062f86545870478b2b8181f6e31
michael <kimh060612@khu.ac.kr>
2021-05-02 17:03:22 +0900

    .

---
+++ 3_RNN/model/attention.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow import keras
+
+class Encoder(keras.Model):
+    def __init__(self):
+        
+        pass
+
+    def call(self, inputs):
+        pass
+
+class Decoder(keras.Model):
+    def __init__(self):
+        
+        pass
+
+    def call(self, inputs):
+        pass
