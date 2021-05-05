07e12fde84a001d4da472904bd27d8b3f73dc70e
michael <kimh060612@khu.ac.kr>
2021-05-05 16:49:16 +0900

    .

---
+++ 3_RNN/transformer/model/model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+class PositionalEncoding(keras.Model):
+    def __init__(self):
+        super(PositionalEncoding, self).__init__()
+    
+    def call(self, inputs):
+        
+        pass
+
+class Attention(keras.Model):
+    def __init__(self):
+        super(Attention, self).__init__()
+        pass
+
+    def call(self, inputs):
+        pass
+
+class MultiHeadAttention(keras.Model):
+    def __init__(self):
+        super(SelfAttention, self).__init__()
+
+        pass
+
+    def call(self, inputs):
+        pass
