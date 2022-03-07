0393b2f880149de88eea81d3f06b4ba715bb4ba3
Michael <kimh060612@naver.com>
2022-03-07 23:33:28 +0900

    .

--- 3_RNN/Basic/RNN/model/model.py
+++ 3_RNN/Basic/RNN/model/model.py
@@ -1,11 +1,18 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
+from torch import dropout
 
 class RNNLayer(keras.Model):
-    def __init__(self, num_hidden=128):
-        super().__init__()
-        
+    def __init__(self, num_hidden=128, num_class=39):
+        super(RNNLayer, self).__init__()
+        self.RNN1 = keras.layers.SimpleRNN(num_hidden)
+        self.RNN2 = keras.layers.SimpleRNN(num_hidden)
+        self.out = keras.layers.Dense(num_class, activation="softmax")
+        self.out = keras.layers.TimeDistributed(self.out)
         
     def call(self, x):
-        pass
+        x = self.RNN1(x)
+        x = self.RNN2(x)
+        out = self.out(x)
+        return out
--- 3_RNN/Basic/RNN/train.py
+++ 3_RNN/Basic/RNN/train.py
@@ -2,3 +2,19 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 
+data_url = "https://homl.info/shakespeare"
+file_path = keras.utils.get_file("shakespeare.txt", data_url)
+
+with open(file_path, "r") as file:
+    text_file = file.read()
+
+tokenizer = keras.preprocessing.text.Tokenizer(char_level=True)
+tokenizer.fit_on_text(text_file)
+
+max_id = len(tokenizer.word_index)
+data_size = tokenizer.document_size
+
+[encoded] = np.array(tokenizer.texts_to_sequences([text_file])) - 1
+
+
+    
